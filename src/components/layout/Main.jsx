3ceb92dad06e4a9e78440f0c14854eee62b92a56
Anton Eliseev <anton_saintp@mail.ru>
2024-03-15 13:19:15 +0300

    category filter

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,33 +1,41 @@
-import React from "react";
-import Movies from "../Movies";
-import Preloader from "../Preloader";
-import Search from "../Search";
+import React from 'react';
+import Movies from '../Movies';
+import Preloader from '../Preloader';
+import Search from '../Search';
 
 export default class Main extends React.Component {
-  state = {
-    movies: [],
-  };
+    state = {
+        movies: [],
+    };
 
-  componentDidMount() {
-    fetch("http://www.omdbapi.com/?apikey=1dcf3d3c&s=matrix")
-      .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search }));
-  }
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=1dcf3d3c&s=matrix')
+            .then((response) => response.json())
+            .then((data) => this.setState({ movies: data.Search }));
+    }
 
-  searchMovies = (string) => {
-    fetch(`http://www.omdbapi.com/?apikey=1dcf3d3c&s=${string}`)
-      .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search }));
-  };
+    searchMovies = (string, type = 'all') => {
+        fetch(
+            `http://www.omdbapi.com/?apikey=1dcf3d3c&s=${string}${
+                type !== 'all' ? `&type=${type}` : ''
+            }`
+        )
+            .then((response) => response.json())
+            .then((data) => this.setState({ movies: data.Search }));
+    };
 
-  render() {
-    const { movies } = this.state;
+    render() {
+        const { movies } = this.state;
 
-    return (
-      <main className="container content">
-        <Search searchMovies={this.searchMovies} />
-        {movies.length ? <Movies movies={this.state.movies} /> : <Preloader />}
-      </main>
-    );
-  }
+        return (
+            <main className='container content'>
+                <Search searchMovies={this.searchMovies} />
+                {movies.length ? (
+                    <Movies movies={this.state.movies} />
+                ) : (
+                    <Preloader />
+                )}
+            </main>
+        );
+    }
 }
